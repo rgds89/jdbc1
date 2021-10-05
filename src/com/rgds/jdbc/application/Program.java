ad0e1a070b3fb225891c0dc3af4ccd8ac3a0507f
Roger Silva <rogersilvati@gmail.com>
2021-10-04 23:48:46 -0300

    Lesson 237 - Get data

--- src/com/rgds/jdbc/application/Program.java
+++ src/com/rgds/jdbc/application/Program.java
@@ -1,12 +1,35 @@
 package com.rgds.jdbc.application;
 
 import com.rgds.jdbc.db.DB;
+import com.rgds.jdbc.db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
-    public static void main(String[] args){
-        Connection connection = DB.getConnection();
-        DB.closeConnection();
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            connection = DB.getConnection();
+            st = connection.createStatement();
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+
     }
 }
--- src/com/rgds/jdbc/db/DB.java
+++ src/com/rgds/jdbc/db/DB.java
@@ -2,9 +2,7 @@ package com.rgds.jdbc.db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -40,6 +38,24 @@ public class DB {
             properties.load(fs);
             return properties;
         } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement stmt) {
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            rs.close();
+        } catch (SQLException e) {
             throw new DbException(e.getMessage());
         }
     }
